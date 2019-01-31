f03087e6177eb4dabfbf87363f4c26bb372b96ab
Eric0329 <ericyang0329@gmail.com>
2019-01-31 20:08:03 +0800

    Update venraaspt.js

--- venraaspt/venraaspt.js
+++ venraaspt/venraaspt.js
@@ -328,20 +328,19 @@ var venraastool = {
 	get_paramValFromURL: function(paramName, url) {
 		var rs = null;
 		
-		var dompath_paramstr = url.split.('?',2);
-		if (dompath_paramstr.length < 2)
-			return rs;
-		
-		var params = dompath_paramstr[1].split('&');
-		for (var i = 0; i < params.length; ++i){
-			var kv = params[i].split('=', 2);
-			if (2 == kv) {
-				if (paramName == kv[0]) {
-					return decodeURIComponent(kv[1].replace(/\+/g, " "));
+		var dompath_paramstr = url.split('?',2);
+		if (2 == dompath_paramstr.length) {
+			var params = dompath_paramstr[1].split('&');
+			for (var i = 0; i < params.length; ++i){
+				var kv = params[i].split('=', 2);
+				if (2 == kv) {
+					if (paramName == kv[0]) {
+						return decodeURIComponent(kv[1].replace(/\+/g, " "));
+					}
 				}
 			}
 		}
-		
+
 		return rs;
 	}
 };
